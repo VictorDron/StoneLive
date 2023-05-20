3a25e4e02597f6ff09c57157a7bbfb67cc165116
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2023-05-20 14:31:10 -0300

    Refatoração 01

--- analysis/base_1.js
+++ analysis/base_1.js
@@ -1,32 +1,48 @@
 const XLSX = require('xlsx');
 const moment = require('moment');
 
-const workbook = XLSX.readFile('./database/base_1.xlsx');
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
-const data = XLSX.utils.sheet_to_json(sheet);
+try {
+  const workbook = XLSX.readFile('./database/base_1.xlsx');
+  const sheet = workbook.Sheets[workbook.SheetNames[0]];
+  const data = XLSX.utils.sheet_to_json(sheet);
 
-data.forEach(row => {
-  row.date = XLSX.SSF.format('yyyy-mm-dd', row.date);
-});
+  data.forEach(row => {
+    if (row.date) {
+      row.date = XLSX.SSF.format('yyyy-mm-dd', row.date);
+    } else {
+      throw new Error('A data não está definida para uma linha.');
+    }
+  });
 
-//Calculo do total de atendimentos
-function getTotalOccurrences(data) {
-  return data.length;
-}
-const totalOccurrences = getTotalOccurrences(data);
+  //Calculo do total de atendimentos
+  function getTotalOccurrences(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
+    }
+    return data.length;
+  }
 
-//Exibe o total de dias analisados no console
-function getTotalDays(data) {
-  const uniqueDates = [...new Set(data.map(row => row.date))];
-  return uniqueDates.length;
-}
-const totalDays = getTotalDays(data);
+  const totalOccurrences = getTotalOccurrences(data);
 
-//Coleta tratada de datas
-function getAllDates(data) {
+  //Exibe o total de dias analisados no console
+  function getTotalDays(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
+    }
+    const uniqueDates = [...new Set(data.map(row => row.date))];
+    return uniqueDates.length;
+  }
+
+  const totalDays = getTotalDays(data);
+
+  //Coleta tratada de datas
+  function getAllDates(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
+    }
     const dates = data.map(row => row.date);
     const count = {};
-  
+
     dates.forEach(date => {
       if (count[date]) {
         count[date]++;
@@ -34,254 +50,286 @@ function getAllDates(data) {
         count[date] = 1;
       }
     });
-  
+
     return count;
-}
-const alldates = getAllDates(data);
+  }
 
-//Calculo da média mensal de atendimentos
-function getMonthlyAverage(data) {
-  const countsByMonth = {};
+  const alldates = getAllDates(data);
 
-  // Agrupar as ocorrências por mês
-  data.forEach(row => {
-    const date = moment(row.date);
-    const month = date.format('YYYY-MM');
-
-    if (!countsByMonth[month]) {
-      countsByMonth[month] = 0;
+  //Calculo da média mensal de atendimentos
+  function getMonthlyAverage(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
     }
+    const countsByMonth = {};
 
-    countsByMonth[month]++;
-  });
+    // Agrupar as ocorrências por mês
+    data.forEach(row => {
+      if (!row.date) {
+        throw new Error('A data não está definida para uma linha.');
+      }
+      const date = moment(row.date);
+      const month = date.format('YYYY-MM');
 
-  // Calcular a média por mês
-  const months = Object.keys(countsByMonth);
-  const monthlyAverages = {};
-  months.forEach(month => {
-    const count = countsByMonth[month];
-    const daysInMonth = moment(month).daysInMonth();
-    const average = count / daysInMonth;
-    monthlyAverages[month] = average.toFixed(2);
-  });
+      if (!countsByMonth[month]) {
+        countsByMonth[month] = 0;
+      }
 
-  return monthlyAverages;
-}
-const monthlyAverages = getMonthlyAverage(data);
+      countsByMonth[month]++;
+    });
 
-//Retorna a média do total de meses em análise
-function getTotalMonthlyAverage(monthlyAverages) {
-  const months = Object.keys(monthlyAverages);
-  const totalAverage = months.reduce((sum, month) => {
-    const average = parseFloat(monthlyAverages[month]);
-    return sum + average;
-  }, 0);
-  const totalMonths = months.length;
-  const totalMonthlyAverage = (totalAverage / totalMonths).toFixed(2);
-  return parseFloat(totalMonthlyAverage);
-}
-const totalMonthlyAverage = getTotalMonthlyAverage(monthlyAverages);
+    // Calcular a média por mês
+    const months = Object.keys(countsByMonth);
+    const monthlyAverages = {};
+    months.forEach(month => {
+      const count = countsByMonth[month];
+      const daysInMonth = moment(month).daysInMonth();
+      const average = count / daysInMonth;
+      monthlyAverages[month] = average.toFixed(2);
+    });
 
+    return monthlyAverages;
+  }
 
-//Calculo da média diária de atendimentos
-function getDailyAverage(data) {
-  const totalOccurrences = getTotalOccurrences(data);
-  const totalDays = getTotalDays(data);
-  const dailyAverage = (totalOccurrences / totalDays).toFixed(2);
-  return parseFloat(dailyAverage);
-}
+  const monthlyAverages = getMonthlyAverage(data);
 
-const dailyAverage = getDailyAverage(data);
-
-//Ordenação crescente das datas
-function sortDates(getAllDates) {
-      const dates = Object.keys(getAllDates);
-      const sortedDates = dates.sort((a, b) => new Date(a) - new Date(b));
-      const sortedObj = {};
-      sortedDates.forEach(date => {
-        sortedObj[date] = getAllDates[date];
-      });
-      return sortedObj;
-}
-const sortedDates = sortDates(alldates);
-
-//Coleta ordenada do Estados presentes no banco
-function getCountryStates(data) {
-  const countryStates = data.map(row => row.country_state);
-  const uniqueCountryStates = [...new Set(countryStates)];
-  return uniqueCountryStates;
-}
-const uniqueCountryStates = getCountryStates(data);
-
-//Coleta ordenada das bases presentes no banco
-function getUniquebase(data) {
-  const base = data.map(row => row.base);
-  const baseReturn = [...new Set(base)];
-  return baseReturn;
-}
-const polo = getUniquebase(data);
-
-//Retorno do número de ocorrências por Estado
-function countCountryStates(data) {
-  const countryStates = data.map(row => row.country_state);
-  const count = {};
-
-  countryStates.forEach(state => {
-    if (count[state]) {
-      count[state]++;
-    } else {
-      count[state] = 1;
+  //Retorna a média do total de meses em análise
+  function getTotalMonthlyAverage(monthlyAverages) {
+    if (typeof monthlyAverages !== 'object') {
+      throw new Error('O argumento fornecido não é um objeto.');
     }
-  });
+    const months = Object.keys(monthlyAverages);
+    const totalAverage = months.reduce((sum, month) => {
+      const average = parseFloat(monthlyAverages[month]);
+      return sum + average;
+    }, 0);
+    const totalMonths = months.length;
+    const totalMonthlyAverage = (totalAverage / totalMonths).toFixed(2);
+    return parseFloat(totalMonthlyAverage);
+  }
 
-  return count;
-}
-const stateCounts = countCountryStates(data);
-//Retorno do número de ocorrências por Base
-function countBases(data) {
-  const bases = data.map(row => row.base);
-  const count = {};
+  const totalMonthlyAverage = getTotalMonthlyAverage(monthlyAverages);
 
-  bases.forEach(base => {
-    if (count[base]) {
-      count[base] += 1;
-    } else {
-      count[base] = 1;
+  //Calculo da média diária de atendimentos
+  function getDailyAverage(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
     }
-  });
+    const totalOccurrences = getTotalOccurrences(data);
+    const totalDays = getTotalDays(data);
+    const dailyAverage = (totalOccurrences / totalDays).toFixed(2);
+    return parseFloat(dailyAverage);
+  }
 
-  return count;
-}
-const baseCount = countBases(data);
+  const dailyAverage = getDailyAverage(data);
 
-//Pesquisa de atendimentos com base na Data
-function getOccurrenceByDate(data, date) {
-    const filteredData = data.filter(row => row.date === date);
-    const statesCount = countCountryStates(filteredData);
-    const basesCount = countBases(filteredData);
-  
-    return {
-      states: statesCount,
-      bases: basesCount
-    };
-}
-const result = getOccurrenceByDate(data, '2020-06-05');
-
-//Retorna a ocorrência das bases por datas
-function getBaseCountsByDate(data) {
-  const countsByDate = {};
-
-  // Agrupar as ocorrências por data
-  data.forEach(row => {
-    const date = row.date;
-    const base = row.base;
-
-    if (!countsByDate[date]) {
-      countsByDate[date] = {};
+  //Ordenação crescente das datas
+  function sortDates(getAllDates) {
+    if (typeof getAllDates !== 'object') {
+      throw new Error('O argumento fornecido não é um objeto.');
     }
+    const dates = Object.keys(getAllDates);
+    const sortedDates = dates.sort((a, b) => new Date(a) - new Date(b));
+    const sortedObj = {};
+    sortedDates.forEach(date => {
+      sortedObj[date] = getAllDates[date];
+    });
+    return sortedObj;
+  }
 
-    if (!countsByDate[date][base]) {
-      countsByDate[date][base] = 1;
-    } else {
-      countsByDate[date][base]++;
+  const sortedDates = sortDates(alldates);
+
+  //Coleta ordenada do Estados presentes no banco
+  function getCountryStates(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
     }
-  });
+    const countryStates = data.map(row => row.country_state);
+    const uniqueCountryStates = [...new Set(countryStates)];
+    return uniqueCountryStates;
+  }
 
-  return countsByDate;
-}
-const baseCountsByDate = getBaseCountsByDate(data);
+  const uniqueCountryStates = getCountryStates(data);
 
-//Verifica o top e o Botton em número atendimentos
-function findMostAndLeastOccurredStates(data) {
+  //Coleta ordenada das bases presentes no banco
+  function getUniquebase(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
+    }
+    const base = data.map(row => row.base);
+    const baseReturn = [...new Set(base)];
+    return baseReturn;
+  }
+
+  const polo = getUniquebase(data);
+
+  //Retorno do número de ocorrências por Estado
+  function countCountryStates(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
+    }
+    const countryStates = data.map(row => row.country_state);
+    const count = {};
+
+    countryStates.forEach(state => {
+      if (count[state]) {
+        count[state]++;
+      } else {
+        count[state] = 1;
+      }
+    });
+
+    return count;
+  }
+
   const stateCounts = countCountryStates(data);
 
-  let mostOccurredState = '';
-  let leastOccurredState = '';
-  let mostOccurrences = 0;
-  let leastOccurrences = Infinity;
+  //Retorno do número de ocorrências por Base
+  function countBases(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
+    }
+    const bases = data.map(row => row.base);
+    const count = {};
 
-  for (const state in stateCounts) {
-    if (stateCounts[state] > mostOccurrences) {
-      mostOccurredState = state;
-      mostOccurrences = stateCounts[state];
-    }
-    if (stateCounts[state] < leastOccurrences) {
-      leastOccurredState = state;
-      leastOccurrences = stateCounts[state];
-    }
+    bases.forEach(base => {
+      if (count[base]) {
+        count[base] += 1;
+      } else {
+        count[base] = 1;
+      }
+    });
+
+    return count;
   }
 
-  return {
+  const baseCount = countBases(data);
+
+  //Retorna a ocorrência das bases por datas
+  function getBaseCountsByDate(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
+    }
+    const countsByDate = {};
+
+    // Agrupar as ocorrências por data
+    data.forEach(row => {
+      if (!row.date) {
+        throw new Error('A data não está definida para uma linha.');
+      }
+      const date = row.date;
+      const base = row.base;
+
+      if (!countsByDate[date]) {
+        countsByDate[date] = {};
+      }
+
+      if (!countsByDate[date][base]) {
+        countsByDate[date][base] = 1;
+      } else {
+        countsByDate[date][base]++;
+      }
+    });
+
+    return countsByDate;
+  }
+
+  const baseCountsByDate = getBaseCountsByDate(data);
+
+  //Verifica o top e o Botton em número atendimentos
+  function findMostAndLeastOccurredStates(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
+    }
+    const stateCounts = countCountryStates(data);
+
+    let mostOccurredState = '';
+    let leastOccurredState = '';
+    let mostOccurrences = 0;
+    let leastOccurrences = Infinity;
+
+    for (const state in stateCounts) {
+      if (stateCounts[state] > mostOccurrences) {
+        mostOccurredState = state;
+        mostOccurrences = stateCounts[state];
+      }
+      if (stateCounts[state] < leastOccurrences) {
+        leastOccurredState = state;
+        leastOccurrences = stateCounts[state];
+      }
+    }
+
+    return {
+      mostOccurredState,
+      leastOccurredState
+    };
+  }
+
+  const { mostOccurredState, leastOccurredState } = findMostAndLeastOccurredStates(data);
+
+  //Verifica a quantidade de bases por Estado
+  function countBasesByState(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('Os dados fornecidos não são um array.');
+    }
+    const stateBases = {};
+
+    data.forEach(row => {
+      const state = row.country_state;
+      const base = row.base;
+
+      if (!stateBases[state]) {
+        stateBases[state] = [base];
+      } else {
+        stateBases[state].push(base);
+      }
+    });
+
+    const stateBaseCount = {};
+
+    for (const state in stateBases) {
+      stateBaseCount[state] = new Set(stateBases[state]).size;
+    }
+
+    return stateBaseCount;
+  }
+
+  const stateBaseCount = countBasesByState(data);
+
+  //Calcula a distribuição percentual de atendimentos entre os Estados
+  function calculateStatePercentage(states) {
+    if (typeof states !== 'object') {
+      throw new Error('O argumento fornecido não é um objeto.');
+    }
+    const total = Object.values(states).reduce((acc, val) => acc + val, 0);
+    const percentages = {};
+    for (let state in states) {
+      const percentage = ((states[state] / total) * 100).toFixed(3) + '%';
+      percentages[state] = percentage;
+    }
+    return percentages;
+  }
+
+  const statePercentages = calculateStatePercentage(stateCounts);
+
+  module.exports = {
+    totalOccurrences,
+    totalDays,
+    alldates,
+    monthlyAverages,
+    totalMonthlyAverage,
+    dailyAverage,
+    sortedDates,
+    uniqueCountryStates,
+    polo,
+    stateCounts,
+    baseCount,
+    baseCountsByDate,
     mostOccurredState,
-    leastOccurredState
+    leastOccurredState,
+    stateBaseCount,
+    statePercentages,
   };
+} catch (error) {
+  console.error('Ocorreu um erro ao processar os dados:', error.message);
 }
-const { mostOccurredState, leastOccurredState } = findMostAndLeastOccurredStates(data);
-
-//Verifica a quantidade de bases por Estado
-function countBasesByState(data) {
-  const stateBases = {};
-
-  data.forEach(row => {
-    const state = row.country_state;
-    const base = row.base;
-
-    if (!stateBases[state]) {
-      stateBases[state] = [base];
-    } else {
-      stateBases[state].push(base);
-    }
-  });
-
-  const stateBaseCount = {};
-
-  for (const state in stateBases) {
-    stateBaseCount[state] = new Set(stateBases[state]).size;
-  }
-
-  return stateBaseCount;
-}
-const stateBaseCount = countBasesByState(data);
-
-//Calcula a distribuição percentual de atendimentos entre os Estados
-function calculateStatePercentage(states) {
-  const total = Object.values(states).reduce((acc, val) => acc + val, 0);
-  const percentages = {};
-  for (let state in states) {
-    const percentage = ((states[state] / total) * 100).toFixed(3) + '%';
-    percentages[state] = percentage;
-  }
-  return percentages;
-}
-const statePercentages = calculateStatePercentage(stateCounts);
-
-
-module.exports = {
-  totalOccurrences,
-  totalDays,
-  alldates,
-  monthlyAverages,
-  totalMonthlyAverage,
-  dailyAverage,
-  sortedDates,
-  uniqueCountryStates,
-  polo,
-  stateCounts,
-  baseCount,
-  result,
-  baseCountsByDate,
-  mostOccurredState,
-  leastOccurredState,
-  stateBaseCount,
-  statePercentages,
-};
-
-
-
-
-
-
-
-
-
-
--- analysis/base_2.js
+++ analysis/base_2.js
@@ -1,99 +1,114 @@
 const XLSX = require('xlsx');
 
-const workbook = XLSX.readFile('./database/base_2.xlsx');
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
-const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+try {
+  const workbook = XLSX.readFile('./database/base_2.xlsx');
+  const sheet = workbook.Sheets[workbook.SheetNames[0]];
+  const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
-//Retorna os valores da coluna base
-function getBaseColumn(data) {
-  const baseColumn = data.slice(1).map(row => row[0]);
-  return baseColumn;
-}
-const BaseColumn = getBaseColumn(data)
-//Retorna os valores da coluna de estoque
-function getStockColumn(data) {
-  const stockColumn = data.slice(1).map(row => row[1]);
-  return stockColumn;
-}
-const StockColumn = getStockColumn(data)
-//Retorna o total de bases
-function getTotalBases(data) {
-  const baseColumn = getBaseColumn(data);
-  return baseColumn.length;
-}
-const TotalBases = getTotalBases(data)
-//Retorna total em estoque
-function getTotalStock(data) {
-  const stockColumn = getStockColumn(data);
-  const totalStock = stockColumn.reduce((total, stock) => total + stock, 0);
-  return totalStock;
-}
-const TotalStock = getTotalStock(data)
-//Retorna a correlação necessária para que a função "calculateStockDistribution" possa ter sucesso
-function getColumns(data) {
+  //Retorna os valores da coluna base
+  function getBaseColumn(data) {
+    const baseColumn = data.slice(1).map(row => row[0]);
+    return baseColumn;
+  }
+
+  const BaseColumn = getBaseColumn(data);
+
+  //Retorna os valores da coluna de estoque
+  function getStockColumn(data) {
+    const stockColumn = data.slice(1).map(row => row[1]);
+    return stockColumn;
+  }
+
+  const StockColumn = getStockColumn(data);
+
+  //Retorna o total de bases
+  function getTotalBases(data) {
+    const baseColumn = getBaseColumn(data);
+    return baseColumn.length;
+  }
+
+  const TotalBases = getTotalBases(data);
+
+  //Retorna total em estoque
+  function getTotalStock(data) {
+    const stockColumn = getStockColumn(data);
+    const totalStock = stockColumn.reduce((total, stock) => total + stock, 0);
+    return totalStock;
+  }
+
+  const TotalStock = getTotalStock(data);
+
+  //Retorna a correlação necessária para que a função "calculateStockDistribution" possa ter sucesso
+  function getColumns(data) {
+    if (data.length < 2 || data[0].length < 2) {
+      throw new Error('O arquivo de dados está vazio ou não possui colunas suficientes.');
+    }
     const baseColumn = data.slice(1).map(row => row[0]);
     const stockColumn = data.slice(1).map(row => row[1]);
     return { bases: baseColumn, stocks: stockColumn };
-}
-//Retorna a distribuição em percentual de estoque nas bases
-function calculateStockDistribution(data) {
+  }
+
+  //Retorna a distribuição em percentual de estoque nas bases
+  function calculateStockDistribution(data) {
     const { bases, stocks } = getColumns(data);
-  
+
     const stockByBase = {};
-  
+
     for (let i = 0; i < bases.length; i++) {
       const base = bases[i];
       const stock = stocks[i];
-  
+
       if (!stockByBase[base]) {
         stockByBase[base] = 0;
       }
-  
+
       stockByBase[base] += stock;
     }
-  
+
     const totalStock = Object.values(stockByBase).reduce((acc, stock) => acc + stock, 0);
-  
+
     const distribution = {};
-  
+
     for (let base in stockByBase) {
       const stock = stockByBase[base];
       const percent = (stock / totalStock) * 100;
       distribution[base] = percent.toFixed(2) + '%';
     }
-  
+
     return distribution;
-}
-const stockDistribution = calculateStockDistribution(data);
-
-function calculateStockByBase(data) {
-  const { bases, stocks } = getColumns(data);
-
-  const stockByBase = {};
-
-  for (let i = 0; i < bases.length; i++) {
-    const base = bases[i];
-    const stock = stocks[i];
-
-    if (!stockByBase[base]) {
-      stockByBase[base] = 0;
-    }
-
-    stockByBase[base] += stock;
   }
 
-  return stockByBase;
+  const stockDistribution = calculateStockDistribution(data);
+
+  function calculateStockByBase(data) {
+    const { bases, stocks } = getColumns(data);
+
+    const stockByBase = {};
+
+    for (let i = 0; i < bases.length; i++) {
+      const base = bases[i];
+      const stock = stocks[i];
+
+      if (!stockByBase[base]) {
+        stockByBase[base] = 0;
+      }
+
+      stockByBase[base] += stock;
+    }
+
+    return stockByBase;
+  }
+
+  const stockByBase = calculateStockByBase(data);
+
+  module.exports = {
+    BaseColumn,
+    StockColumn,
+    TotalBases,
+    TotalStock,
+    stockDistribution,
+    stockByBase
+  };
+} catch (error) {
+  console.error('Ocorreu um erro ao processar os dados:', error.message);
 }
-const stockByBase = calculateStockByBase(data);
-
-module.exports = {
-BaseColumn,
-StockColumn,
-TotalBases,
-TotalStock,
-stockDistribution,
-stockByBase
-};
-
-
-
